491995582d24ff8579701325d16980c5347dd47a
Admin <dmitrijdundakov@gmail.com>
2021-10-06 22:49:15 +0300

    first attempt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        
+        for (int i = 0; i < 7; i++) {
+            System.out.println("I hate Physics");
+        }
     }
 }
